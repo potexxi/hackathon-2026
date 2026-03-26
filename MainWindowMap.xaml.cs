404c4169d91a543dd6ce3e0864607a0799c3d7a7
ITzZysn <macsaipeti2010@gmail.com>
2026-03-26 10:04:03 +0100

    commit

--- MainWindowMap.xaml.cs
+++ MainWindowMap.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Mapsui;
 using Mapsui.Projections;
 using Mapsui.Tiling;
+using Mapsui.UI.Wpf;
+using Mapsui.Widgets;
+using Mapsui.Widgets.InfoWidgets;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +18,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
 namespace Hackathon
 {
     /// <summary>
@@ -25,16 +29,27 @@ namespace Hackathon
         public MainWindowMap()
         {
             InitializeComponent();
-            // Karte erstellen
-            mapControl.Map = new Map();
+            LoggingWidget.ShowLoggingInMap = ActiveMode.No;
+            Map map = new Map();
+            // OSM-Tile-Layer
+            map.Layers.Add(OpenStreetMap.CreateTileLayer());
 
-            // OpenStreetMap Layer hinzufügen
-            mapControl.Map.Layers.Add(OpenStreetMap.CreateTileLayer());
+            map.Layers.Add(OpenStreetMap.CreateTileLayer());
 
-            // Startposition (Wien)
-            var position = SphericalMercator.FromLonLat(16.3738, 48.2082);
-            
-            
+            // Map dem Control zuweisen
+            mapControl.Map = map;
+
+            // Wien Koordinaten
+            var position = SphericalMercator.FromLonLat(9.7415, 47.4125);
+
+            mapControl.Map.Navigator.OverrideZoomBounds = new MMinMax(10, 1000);
+            // 👉 WICHTIG: Navigator benutzen (ohne Resolutions!)
+            mapControl.Map.Navigator.CenterOn(position.x, position.y);
+
+            // Zoom setzen (z. B. 1000 = näher, 10000000 = weiter raus)
+            mapControl.Map.Navigator.ZoomTo(10);
+
+
 
         }
     }
