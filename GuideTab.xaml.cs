b9223dce5648b176df357a02cd9724d019fde7fb
K D <karim.debbabi4@gmail.com>
2026-03-26 12:57:49 +0100

    commit12

--- GuideTab.xaml.cs
+++ GuideTab.xaml.cs
@@ -23,5 +23,10 @@ namespace Hackathon
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
