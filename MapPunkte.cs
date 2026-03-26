2552d9c657ad5c48bdd617a45a83174fcac06c52
K D <karim.debbabi4@gmail.com>
2026-03-26 16:13:53 +0100

    Update MapPunkte.cs

--- MapPunkte.cs
+++ MapPunkte.cs
@@ -2,6 +2,7 @@
 using Mapsui.Projections;
 using Mapsui.Styles;
 using Mapsui.UI.Wpf;
+using Org.BouncyCastle.Bcpg.Sig;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,19 +15,41 @@ namespace Hackathon
     {
         public double lon = 9.7415;
         public double lat = 47.4125;
+        public string color;
 
         public MapPunkte() {}
-        public MapPunkte(double lon, double lat) 
+        public MapPunkte(double lon, double lat, string color) 
         {
             this.lon = lon;
             this.lat = lat;
+            this.color = color;
         }
 
         public PointFeature PunktErstellen()
         {
             var point = SphericalMercator.FromLonLat(lon, lat);
             PointFeature feature = new PointFeature(point);
-            feature.Styles.Add(new SymbolStyle { SymbolType = SymbolType.Ellipse, Fill = new Mapsui.Styles.Brush(new Mapsui.Styles.Color(255, 0, 0)), SymbolScale = 0.3, Outline = new Mapsui.Styles.Pen { Color = new Mapsui.Styles.Color(255, 255, 255, 0), Width = 0 } });
+            if (color.ToLower() == "red")
+            {
+                feature.Styles.Add(new SymbolStyle
+                {
+                    SymbolType = SymbolType.Ellipse,
+                    Fill = new Mapsui.Styles.Brush(new Mapsui.Styles.Color(255, 0, 0)),
+                    SymbolScale = 0.3,
+                    
+                });
+            }
+            else
+            {
+                feature.Styles.Add(new SymbolStyle
+                {
+                    SymbolType = SymbolType.Ellipse,
+                    Fill = new Mapsui.Styles.Brush(new Mapsui.Styles.Color(0, 0, 255)),
+                    SymbolScale = 0.3,
+                    
+                });
+                
+            }
             return feature;
         }
 
